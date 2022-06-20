0f7242a1c15e39a70b4251253f5860dbbb6dc4e1
ujwalgb <67465790+ujwalgb@users.noreply.github.com>
2022-06-20 12:10:52 +0530

    Update signup.jsx

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -65,7 +65,7 @@ function signup() {
     <div className={styles.signup}>
       <div className={styles.signup_form__box}>
         <div className={styles.signup__form__content}>
-          <h2>Welcome to Design Community </h2>
+          <h2>Welcome to Leafnow </h2>
           <p className="flex gap-2 text-gray-500">
             Already have an ccount?{" "}
             <Link href="/login">
